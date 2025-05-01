5361d66c79bdd6fe16060c94e15ea6a9c5ecfc16
RicardoBassi <bassidericardo@gmail.com>
2025-05-01 18:23:25 +0200

    Alterando settings

--- DjangoAPI/backend/settings.py
+++ DjangoAPI/backend/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vw^00=7)=v3vludw&=am$kf3kxzqlmcl=#$+_#m!l4v$x+859h'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
@@ -116,11 +120,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'prediction_db',            # Database name
-        'USER': 'prediction_user',          # Database user
-        'PASSWORD': 'Senha123!',            # Database password
-        'HOST': 'db',                       # Database host
-        'PORT': '5432',                     # PostgreSQL port
+        'NAME': os.getenv("DB_NAME"),          # Database name
+        'USER': os.getenv("DB_USER"),          # Database user
+        'PASSWORD': os.getenv("DB_PASSWORD"),  # Database password
+        'HOST': os.getenv("DB_HOST"),          # Database host
+        'PORT': os.getenv("DB_PORT"),          # PostgreSQL port
     }
 }
 
